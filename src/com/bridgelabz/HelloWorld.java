85dd50de4bfb7e9d5718dd46c7fb719e05858560
Sagar Pratap Singh <sagarpratapsingh9432@gmail.com>
2023-04-03 18:17:41 +0530

    Second commit

--- src/com/bridgelabz/HelloWorld.java
+++ src/com/bridgelabz/HelloWorld.java
@@ -3,5 +3,6 @@ package com.bridgelabz;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello java");
+        System.out.println("Hi this is new");
     }
 }
